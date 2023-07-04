fb13462e9b4a6682442b3c7ef74f5dbce278caca
Onur Doğan <crrccnt@pm.me>
2023-07-04 15:40:45 +0300

    First Commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-nureply-blue text-8xl">
+        Nureply
+      </h1>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'nureply-blue': 'rgba(59,130,246,.5)',
+      },
+    },
+  },
+  plugins: [],
+}
